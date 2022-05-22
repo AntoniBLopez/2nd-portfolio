0e6da1aaae7f1327852aac6132876800db3fce47
Toni B.Lopez local <tonibassols@outlook.es>
2022-05-22 07:14:40 +0200

    I add a header & home sections

--- docs/app/app.js
+++ docs/app/app.js
@@ -1,19 +1,19 @@
-const links = document.querySelectorAll('.link');
-const sections = document.querySelectorAll('section');
+// const links = document.querySelectorAll('.link');
+// const sections = document.querySelectorAll('section');
 
-let activeLink = 0;
+// let activeLink = 0;
 
-links.forEach((link, i) => {
-    link.addEventListener('click', () => {
-        if(activeLink != i){
-            links[activeLink].classList.remove('active');
-            link.classList.add('active');
-            sections[activeLink].classList.remove('active');
+// links.forEach((link, i) => {
+//     link.addEventListener('click', () => {
+//         if(activeLink != i){
+//             links[activeLink].classList.remove('active');
+//             link.classList.add('active');
+//             sections[activeLink].classList.remove('active');
 
-            setTimeout(() => {
-                activeLink = i;
-                sections[i].classList.add('active');
-            }, 1000);
-        }
-    })
-})
+//             setTimeout(() => {
+//                 activeLink = i;
+//                 sections[i].classList.add('active');
+//             }, 1000);
+//         }
+//     })
+// })

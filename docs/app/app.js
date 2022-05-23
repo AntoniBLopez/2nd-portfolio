bcfe5f161d9641662f383c9a5b966c423ad24182
Toni B.Lopez local <tonibassols@outlook.es>
2022-05-23 13:11:42 +0200

    I add a skills & projects

--- docs/app/app.js
+++ docs/app/app.js
@@ -1,3 +1,10 @@
+const header = document.querySelector('header')
+
+window.addEventListener ('scroll', function() {
+    header.classList.toggle('sticky', window.scrollY > 0)
+})
+
+
 // const links = document.querySelectorAll('.link');
 // const sections = document.querySelectorAll('section');
 

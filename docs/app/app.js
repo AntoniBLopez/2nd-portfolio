1a24a27efe29deff406d550cff7348532b38693d
Toni B.Lopez local <tonibassols@outlook.es>
2022-09-27 23:06:21 +0200

    Change the Navlist of the menu

--- docs/app/app.js
+++ docs/app/app.js
@@ -3,9 +3,48 @@ import mainPart from './logic/mainPart.js'
 const header = document.querySelector('header')
 const projectsInfoForMobile = document.getElementById('projectsInfoForMobile')
 
-window.addEventListener ('scroll', () => header.classList.toggle('sticky', window.scrollY > 0))
+window.addEventListener ('scroll', () => {
+    header.classList.toggle('sticky', window.scrollY > 0)
+
+    if (navlist.classList.value === 'navlist') {
+        coverShadow.style.display = 'none'
+
+        if (header.classList.value === 'sticky') {
+            sticky.style.boxShadow = '0px 0px 10px rgb(0 0 0 / 10%)'
+        }
+    }
+})
+window.addEventListener ('click', () => {
+
+    if (navlist.classList.value === 'navlist active') {
+
+        setTimeout(() => {
+
+            if (window.scrollY === 0) {
+                coverShadow.style.top = '56px'
+            } else if (window.scrollY > 0) {
+                coverShadow.style.top = '46px'
+            }
+
+            coverShadow.style.display = 'flex'
+
+        }, 500);
+
+        if (header.classList.value === 'sticky') {
+            sticky.style.boxShadow = 'none'
+        }
+    } else if (navlist.classList.value === 'navlist') {
+            coverShadow.style.display = 'none'
+
+        if (header.classList.value === 'sticky') {
+            sticky.style.boxShadow = '0px 0px 10px rgb(0 0 0 / 10%)'
+        }
+    }
+})
 
 const menu = document.querySelector('#menu-icon')
+const sticky = document.querySelector('.sticky')
+const coverShadow = document.querySelector('.coverShadow')
 const navlist = document.querySelector('.navlist')
 
 menu.onclick = () => {
@@ -17,6 +56,7 @@ window.onscroll = () => {
     menu.classList.remove('bx-x')
     navlist.classList.remove('active')
 }
+
 
 // Transition efect
 const sr = ScrollReveal ({
@@ -35,13 +75,16 @@ sr.reveal('.contact-section', {delay:200, origin:'bottom'})
 // Esta lógica de a continuación que es info. para una buena experiencia de usuario con interacción, ponerlo en otro archivo y añadirlo aquí modularizado
 sr.reveal('#projectsInfoForMobile', {delay:1000, duration: 2000, origin: 'top', distance: '100px', beforeReveal: checkIfIsMobileDevice, afterReveal: hideEffect})
 
+
 const pageWidth  = document.documentElement.scrollWidth;
+
 function checkIfIsMobileDevice (el) {
 
     if (pageWidth > 950) {
         projectsInfoForMobile.style.display = 'none'
     }
 }
+
 
 function hideEffect (el) {
 
